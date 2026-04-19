1863968422a80ec6759c93c1f830a823d29bff13
wen-063 <260320352+wen-063@users.noreply.github.com>
2026-04-19 19:35:22 +0800

    加密钥

--- myblog/blog.py
+++ myblog/blog.py
@@ -12,6 +12,7 @@ from datetime import datetime, timezone
 load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', '123456')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
